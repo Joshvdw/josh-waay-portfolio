16d1967db0b5e518d67fa0fe3de6103f45c582aa
Joshvdw <joshvdw@live.com>
2025-05-15 13:16:46 +1200

    fixes

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,16 +20,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.className} ${baj_jamjuree.variable}`}>
-      <head>
-        {/* <link rel="prefetch" href="/lotties/preloader_star.json" as="fetch" />
+      {/* <head>
+        <link rel="prefetch" href="/lotties/preloader_star.json" as="fetch" />
         <link rel="prefetch" href="/lotties/hero_star.json" as="fetch" />
         <link rel="prefetch" href="/lotties/skip_lottie.json" as="fetch" />
         <link
           rel="prefetch"
           href="/lotties/play-pause_lottie.json"
           as="fetch"
-        />*/}
-      </head>
+        />
+      </head> */}
       <body>{children}</body>
     </html>
   );
